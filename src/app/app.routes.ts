3fe712cb13b315981b048dc9122592eb9164259a
Mohamed Aziz Hafhouf <mohamedazizhafhouf7@gmail.com>
2026-03-23 17:06:49 +0100

    feat(Login): login UI, routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/auth/login/login';
+import { LandingPage } from './components/landing-page/landing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component:Login},
+    {path: '', component:LandingPage}
+];
--- src/app/components/auth/login/login.ts
+++ src/app/components/auth/login/login.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ErrorTooltip } from '../../error-tooltip/error-tooltip';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, CommonModule, ReactiveFormsModule, ErrorTooltip],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+// Reusing the showPassword signal for the eye icon
+  showPassword = signal(false);
+
+  // Define the login form with basic validation
+  loginForm = new FormGroup({
+    email: new FormControl('', { validators: [Validators.email, Validators.required], updateOn: 'blur'}),
+    password: new FormControl('', [Validators.required])
+  });
+
+  // Toggle password visibility
+  togglePassword() {
+    this.showPassword.update(v => !v);
+  }
+
+  // Handle form submission
+  onLogin() {
+    if (this.loginForm.valid) {
+      // Calling the authService
+    } else {
+      // Mark all fields as touched to show errors
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  // Helper to check for errors (used for border color)
+  isInvalid(path: string): boolean {
+    const control = this.loginForm.get(path);
+    return !!(control && control.invalid && control.touched);
+  }
+}

c61aec8453f36ee8142caea842c8f19f3697ad22
Mohamed Aziz Hafhouf <mohamedazizhafhouf7@gmail.com>
2026-03-23 22:19:27 +0100

    feat(login): updating error handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Login } from './components/auth/login/login';
 import { LandingPage } from './components/landing-page/landing-page';
 import { ForgotPassword } from './components/auth/forgot-password/forgot-password';
+import { Register } from './components/auth/register/register';
 
 export const routes: Routes = [
     {path: 'login', component:Login},
+    {path: 'register', component:Register},
     {path: 'forgot-password', component:ForgotPassword},
 
     {path: '', component:LandingPage}
